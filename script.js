ade5cbfd07ec9691d850815a4073399e750a2898
Helen Darden <hgdarden@gmail.com>
2020-04-30 22:31:04 -0400

    started loop

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //when button is clicked on webpage
 var generateBtn = document.querySelector("#generate");
     
-//condition statements/questions
-var clickHowMany = prompt("How many characters would you like your password to be?");
-// if 8-128 then input is collected, else if not, line 27-32
-var clickSpec = confirm("Click ok to confirm including special characters?");
-var clickNumb = confirm("Click ok to confirm including numbers?");
-var clickLower = confirm("Click ok to confirm including lowercase letters?");
-var clickUpper = confirm("Click ok to confirm including uppercase letters?");
 
 // function object is to generate password
 function generatePassword () {
+    var clickHowMany = prompt("How many characters would you like your password to be?");
+// if 8-128 then input is collected, else if not, line 27-32
+    var clickSpec = confirm("Click ok to confirm including special characters?");
+    var clickNumb = confirm("Click ok to confirm including numbers?");
+    var clickLower = confirm("Click ok to confirm including lowercase letters?");
+    var clickUpper = confirm("Click ok to confirm including uppercase letters?");
 
-  
 //Need an array of upper, lower, all numbers and special characters . 'push' what they chose into the array that they choose. 
 //arrays
-var rUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I","J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var rLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var rNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var rSpecChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+    var rUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I","J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var rLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var rNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var rSpecChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
 
 
 //user input of conditions
@@ -46,14 +46,18 @@ if (clickUpper); {
 }
 
 //collection from user input^^^
-function userInput(){
-  
+
+userInput = []
+console.log(userInput);  
+
 return userInput;
 
-}
 
 
+
 }
+///loop 
+for (var i = 0; i < userInput; i ++)
 
 
 // Write password to the #password input//// added (userInput)
